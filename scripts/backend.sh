bfdabf01ebba991dbab3787f799e3e47a3701a9f
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2020-11-09 16:25:47 -0300

    configurando github action

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker restart backend
+docker-compose -f docker-compose.prod.yaml up -d backend
+docker-compose -f docker-compose.prod.yaml up -d nginx
+docker-compose -f docker-compose.prod.yaml restart backend
+#docker-compose -f docker-compose.prod.yaml restart nginx
