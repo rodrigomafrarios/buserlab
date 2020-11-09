d6659ee4a9c5cb9ebdc13e668e7e148ee489eeef
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2020-11-09 16:03:42 -0300

    fix appspec.yml

--- backend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cp /home/ubuntu/docker-compose.prod.yaml .
-docker-compose -f docker-compose.prod.yaml up -d backend
-docker-compose -f docker-compose.prod.yaml up -d nginx
-docker-compose -f docker-compose.prod.yaml restart backend
-#docker-compose -f docker-compose.prod.yaml restart nginx
