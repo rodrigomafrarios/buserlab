6001b6e28267d01f02f7cbd613ccb047024af4b0
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2020-11-09 16:42:31 -0300

    configurando github action

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo cp /home/ubuntu/docker-compose.prod.yaml /hom/ubuntu/buserlab/docker-compose.prod.yaml
+sudo cp ../docker-compose.prod.yaml /home/ubuntu/buserlab/docker-compose.prod.yaml
 sudo docker-compose -f docker-compose.prod.yaml restart backend
