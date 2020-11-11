08fb374d966ced44689d149a181b45404cbf77ca
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2020-11-11 09:44:43 -0300

    colocando docker pull

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+sudo docker pull rodrigomafra/buserlab_backend
+sudo docker pull rodrigomafra/buserlab_nginx
+sudo docker pull rodrigomafra/buserlab_postgres
 sudo cp ../docker-compose.prod.yaml /home/ubuntu/buserlab/docker-compose.prod.yaml
 sudo docker-compose -f docker-compose.prod.yaml restart backend
